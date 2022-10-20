a44c6f42d01ac13b51a203e052512f8ef2571e23
SierraKill <yoleiru@gmail.com>
2022-10-20 17:27:00 -0600

    Login productos dev

---
+++ layouts/scripts.template.php
@@ -0,0 +1,10 @@
+<!-- JAVASCRIPT -->
+    <script src="../public/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../public/libs/simplebar/simplebar.min.js"></script>
+    <script src="../public/libs/node-waves/waves.min.js"></script>
+    <script src="../public/libs/feather-icons/feather.min.js"></script>
+    <script src="../public/js/pages/plugins/lord-icon-2.1.0.js"></script>
+    <script src="../public/js/plugins.js"></script>
+
+   
+    <script src="../public/js/app.js"></script>
